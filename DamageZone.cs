5e525b9d74eb05ccd5fbd037f1b2921fd30aa7b7
gitmaus1 <63102077+gitmaus1@users.noreply.github.com>
2023-12-07 13:48:28 +0000

    Add files via upload

---
+++ DamageZone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        RubyController controller = other.GetComponent<RubyController>();
+
+        if (controller != null)
+        {
+            // tekur líf
+            controller.ChangeHealth(-1);
+        }
+    }
+
+}
+
